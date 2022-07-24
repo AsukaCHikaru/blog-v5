b604fc98037f9c6415e7ca007fee659d59bc3212
asukachikaru <asukachikaru@gmail.com>
2022-07-24 14:45:19 +0900

    feat: build post list page

---
+++ templates/PostListPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+//template_import_post_link
+//template_import_data
+
+export const PostListPage: React.FC = () => {
+  return (
+  <div>
+    //template_map_post_link
+  </div>)
+}
--- tools/blogFetcher.ts
+++ tools/blogFetcher.ts
@@ -1,3 +1,6 @@
+import {writeFileSync, readFileSync} from 'fs'
+import {resolve} from 'path'
+
 import { PostSummary } from "../types";
 import { getNotionBlockList, getNotionPageList } from "./notionApiService";
 
@@ -6,6 +9,21 @@ const run = async () => {
   try {
     notionPageList = await getNotionPageList();
     console.info('Notion page list fetched.')
+
+    const data = `import { PostSummary } from '../types';
+  
+export const postList: PostSummary[] = ${JSON.stringify(notionPageList)};`
+
+    await writeFileSync(resolve(__dirname, '../src', 'data.ts'), data);
+
+    const rawPostListPage = await readFileSync(resolve(__dirname, '../templates', 'PostListPage.tsx'), {encoding: 'utf-8'})    
+    const postListPage = rawPostListPage
+    .replace("//template_import_post_link", "import { PostLink } from '../components/PostLink';")
+    .replace("//template_import_data", "import { postList } from '../data';")
+    .replace("//template_map_post_link", `{postList.map((postSummary) => <PostLink key={postSummary.pathname} postSummary={postSummary} />)}`);
+    
+    await writeFileSync(resolve(__dirname, '../src', 'pages', 'PostListPage.tsx'), postListPage)
+    
   } catch (error) {
     console.error(error);
   }
@@ -14,7 +32,7 @@ const run = async () => {
     try {
       console.info(`Start fetching post ${page.title}`);
       const post = await getNotionBlockList(page.id)
-      console.log(post);
+      // console.log(post);
       console.info(`Post ${page.title} fetched.`)
     } catch (error) {
       console.error(error);

607468d739c74a37fa1ee10fb73fac002d98e9a6
Asuka Wang <hungmao.wang@linecorp.com>
2022-12-21 23:36:42 +0900

    feat: post summary list fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import { useEffect } from 'react'
+import { NotionPageListResponse } from '../types/notion'
+import { PostCategory, PostLanguage, PostSummary } from '../types'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+interface Props {
+  postSummaryList: PostSummary[] 
+}
+
+export default function Home({postSummaryList}: Props) {
+  
   return (
     <>
       <Head>
@@ -14,110 +21,67 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <div>
+        {postSummaryList.map((postSummary) => {
+          return <div key={postSummary.id}>
+          <Link  href={`/post/${postSummary.pathname}`}>{postSummary.title}</Link>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        })}
+      </div>
     </>
   )
+}
+
+const postListFilterSorter = {
+  filter: {
+    property: "state",
+    select: {
+      equals: "Done",
+    },
+  },
+  sorts: [
+    {
+      property: "published",
+      direction: "descending",
+    },
+  ],
+};
+
+
+export async function getStaticProps () {
+  const res = await fetch(`https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE_ID}/query`, {
+    method: 'POST',
+    headers: new Headers({
+      'Authorization': `Bearer ${process.env.NOTION_API_TOKEN}`,
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(postListFilterSorter),
+  });
+  const notionPageList: NotionPageListResponse = await res.json();
+
+  const postSummaryList: PostSummary[] = [];
+
+  if (notionPageList.results && notionPageList.results.length > 0){
+    notionPageList.results.forEach((result) => {
+      const postSummary: PostSummary = {
+        id: result.id,
+        title: result.properties?.Name.title[0].plain_text || "",
+        category: (result.properties?.category.select.name as PostCategory) || "others",
+        language:(result.properties?.language.multi_select.map(
+              (select) => select.name
+            ) as PostLanguage[]) || [],
+        tags: result.properties?.tags.multi_select.map((select) => select.name) || [],
+        publishDate: result.properties?.published?.date.start || "",
+        pathname: result.properties?.pathname.rich_text[0]?.plain_text || "",
+        zhTwLink: result.properties?.link_zhTW?.url || null,
+      };
+      postSummaryList.push(postSummary);
+    }); 
+  }
+
+  return {
+    props: {
+      postSummaryList,
+    }
+  }
 }

db836418025d5ed4c92814728317f7ecbc9418e3
asukachikaru <asukachikaru@gmail.com>
2022-12-29 12:17:13 +0900

    chore: use local font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Noto_Serif_JP } from "@next/font/google";
-
-const notoSerifJp = Noto_Serif_JP({ weight: ["200", "400", "600", "900"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={`${notoSerifJp.className}`}>
-      <Component {...pageProps} />
-    </main>
-  );
+  return <Component {...pageProps} />;
 }

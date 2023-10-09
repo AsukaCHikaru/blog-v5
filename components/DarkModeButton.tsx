98e287c6536e47d0e40c61c90dc73901b93c6f42
asukachikaru <asukachikaru@gmail.com>
2023-10-09 23:19:12 +0900

    feat: toggle darkmode with dark mode button

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -1,6 +1,13 @@
+import { useDarkMode } from 'hooks/useDarkMode';
+
 export const DarkModeButton = () => {
+  const { toggleDarkMode } = useDarkMode();
+
   return (
-    <button className="whitespace-pre text-right text-xs lg:text-sm leading-4">
+    <button
+      className="whitespace-pre text-right text-xs lg:text-sm leading-4"
+      onClick={toggleDarkMode}
+    >
       dark{`\n`}mode
     </button>
   );

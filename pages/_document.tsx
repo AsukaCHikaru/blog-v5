5a168714dfcf7195d5dc0a79ce0cee89eadf92ea
asukachikaru <asukachikaru@gmail.com>
2023-10-09 23:20:26 +0900

    fix: dark mode flicker issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,17 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script strategy="beforeInteractive" id="theme-document">{`
+        if (typeof window !== "undefined") {
+          const savedTheme = localStorage.getItem("ASUKACHIKARU_THEME");
+          console.log(savedTheme)
+          if (savedTheme === "DARK") {
+            document.documentElement.classList.add('dark');
+          } else {
+            document.documentElement.classList.remove('dark');
+          }
+        }
+      `}</Script>
       </body>
     </Html>
   );

c36d94ddfcb7ebe8ee412583bec76e911f8d03c6
asukachikaru <asukachikaru@gmail.com>
2023-10-09 16:25:48 +0900

    feat: dark mode button

---
+++ components/DarkModeButton.tsx
@@ -0,0 +1,7 @@
+export const DarkModeButton = () => {
+  return (
+    <button className="whitespace-pre text-right text-xs lg:text-sm leading-4">
+      dark{`\n`}mode
+    </button>
+  );
+};
--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,15 +1,19 @@
 import Link from 'next/link';
+import { DarkModeButton } from './DarkModeButton';
 
 export const SiteHeader = () => {
   return (
     <div className="mt-12 mb-6 col-span-10 col-start-2 lg:col-span-8 lg:col-start-3 grid grid-col-10 text-center">
-      <div className="col-span-10 text-xl lg:text-3xl font-extrabold">
+      <div className="col-start-1 col-span-10 text-xl lg:text-3xl font-extrabold">
         Asuka Wang
       </div>
-      <div className="col-span-2 col-start-5 mt-4 flex justify-between">
+      <div className="col-span-2 col-start-5 mt-4 flex justify-between leading-[32px] lg:leading-[40px]">
         <Link href="/blog">BLOG</Link>
         <Link href="/snapshot">SNAPSHOT</Link>
         <Link href="/about">ABOUT</Link>
+      </div>
+      <div className="col-span-1 col-start-10 mt-4 flex justify-end">
+        <DarkModeButton />
       </div>
       <div className="col-span-10 my-2 border-t border-b h-2" />
     </div>

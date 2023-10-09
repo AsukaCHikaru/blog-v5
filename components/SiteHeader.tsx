0007b8662b2b1760bcf41abeb8f85a533a23905c
asukachikaru <asukachikaru@gmail.com>
2023-10-09 23:48:02 +0900

    fix: site header layout

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -3,11 +3,18 @@ import { DarkModeButton } from './DarkModeButton';
 
 export const SiteHeader = () => {
   return (
-    <div className="mt-12 mb-6 col-span-10 col-start-2 lg:col-span-8 lg:col-start-3 grid grid-col-10 text-center">
+    <div className="mt-12 mb-6 col-span-10 col-start-2 lg:col-span-8 lg:col-start-3 grid grid-cols-10 text-center">
       <div className="col-start-1 col-span-10 text-xl lg:text-3xl font-extrabold">
         Asuka Wang
       </div>
-      <div className="col-span-2 col-start-5 mt-4 flex justify-between leading-[32px] lg:leading-[40px]">
+      <div
+        className="
+      col-span-6 col-start-3 lg:col-span-6 lg:col-start-3
+      mt-4 flex justify-between
+      leading-[32px] lg:leading-[40px]
+      text-xs lg:text-base
+      "
+      >
         <Link href="/blog">BLOG</Link>
         <Link href="/snapshot">SNAPSHOT</Link>
         <Link href="/about">ABOUT</Link>

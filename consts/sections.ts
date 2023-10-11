bc378ba6b57814985a1fd9a04d53c0a498518288
asukachikaru <asukachikaru@gmail.com>
2023-10-12 00:02:33 +0900

    feat: define section title and descriptions as const

---
+++ consts/sections.ts
@@ -0,0 +1,17 @@
+export const SECTIONS = {
+  ABOUT: {
+    path: '/about',
+    title: 'ABOUT',
+    description: 'Introduction about me and this site',
+  },
+  BLOG: {
+    path: '/blog',
+    title: 'BLOG',
+    description: 'Introduction about me and this site',
+  },
+  SNAPSHOT: {
+    path: '/snapshot',
+    title: 'SNAPSHOT',
+    description: 'Introduction about me and this site',
+  },
+} as const;

b8de63f59cb0cd3397c420fde174fa720010f9b5
asukachikaru <asukachikaru@gmail.com>
2023-10-14 21:21:49 +0900

    refactor: adjust global layout component location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Noto_Serif_JP } from '@next/font/google';
-import { ThemeLayout } from '@components/blog/layout/ThemeLayout';
-import { GridLayout } from '@components/blog/layout/GridLayout';
+import { ThemeLayout } from '@components/layout/ThemeLayout';
+import { GridLayout } from '@components/layout/GridLayout';
 import { SiteHeader } from '@components/SiteHeader';
 import { Footer } from '@components/blog/Footer';
 

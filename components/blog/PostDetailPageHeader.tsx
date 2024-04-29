fadcc0079e294b80e8be567464e2af55a12beb23
asukachikaru <asukachikaru@gmail.com>
2024-04-29 15:29:46 +0900

    feat: post detail header layout

--- components/blog/PostDetailPageHeader.tsx
+++ components/blog/PostDetailPageHeader.tsx
@@ -9,15 +9,12 @@ interface Props {
 
 export const PostDetailPageHeader: FC<Props> = ({ postSummary }) => {
   return (
-    <div className="mb-6 col-span-10 col-start-2 lg:col-span-8 lg:col-start-3 text-center">
-      <h1 className="mb-2 text-4xl lg:text-6xl lg:leading-tight font-extrabold">
-        {postSummary.title}
+    <div className="flex flex-col gap-fb3 mb-fb5">
+      <h1 className="text-fb11 leading-none">
+        {postSummary.title.toUpperCase()}
       </h1>
-      <div className="text-md lg:text-xl">
-        <span className={`inline-block mr-2 pr-2 ${!!postSummary.tags.length}`}>
-          {parseDateToEn(postSummary.publishDate)}
-        </span>
-      </div>
+      <h2 className="text-fb5 italic">TODO: Post Description</h2>
+      <div className="text-fb3">{parseDateToEn(postSummary.publishDate)}</div>
     </div>
   );
 };

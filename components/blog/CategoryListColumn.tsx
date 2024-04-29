69e9d9192ec0d4446cafda7c87076bab824e616c
asukachikaru <asukachikaru@gmail.com>
2024-04-29 16:10:45 +0900

    feat: CategoryList

---
+++ components/blog/CategoryListColumn.tsx
@@ -0,0 +1,41 @@
+import { CategoryList } from '@types';
+import { FC } from 'react';
+import { SideColumnHeader } from './SideColumnHeader';
+import Link from 'next/link';
+
+interface Props {
+  categoryList: CategoryList;
+}
+
+export const CategoryListColumn: FC<Props> = ({ categoryList }) => {
+  return (
+    <div>
+      <SideColumnHeader>CATEGORY</SideColumnHeader>
+      <div className="flex flex-col gap-fb2 border-l-2 border-light50 pl-fb2">
+        {Object.entries(categoryList)
+          .sort(
+            ([, prevPostCount], [, nextPostCount]) =>
+              nextPostCount - prevPostCount,
+          )
+          .map(([category, postNumber]) => (
+            <div
+              key={`category-list-item-${category}`}
+              className="text-light75 hover:text-light"
+            >
+              <Link
+                href={`/blog?category=${category}`}
+                className="flex leading-fb3 items-end"
+              >
+                <span className="text-fb3">{category}</span>
+                <span className="mx-fb1 flex-grow border-b border-dashed border-light50 hover:border-light" />
+                <span className="text-fb3">{postNumber}</span>
+                <span className="ml-fb1 text-fb2 leading-none">
+                  POST{postNumber > 1 ? 'S' : ''}
+                </span>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};

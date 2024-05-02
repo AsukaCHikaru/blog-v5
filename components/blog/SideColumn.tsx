a01de6da3b9f12d39aba3e7389192e31b5eebe7e
Asuka Wang <hungmao.wang@linecorp.com>
2024-05-02 18:37:47 +0900

    fix: side column top button margin-top

--- components/blog/SideColumn.tsx
+++ components/blog/SideColumn.tsx
@@ -8,7 +8,7 @@ export const SideColumn: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex flex-col justify-between">
       <div className="flex flex-col gap-fb5">{children}</div>
-      <div className="self-end">
+      <div className="self-end mt-fb5">
         <button
           className="font-noto-sans text-fb3 text-light75 hover:text-light"
           onClick={handleTopClick}

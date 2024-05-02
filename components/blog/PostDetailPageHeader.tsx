e83b02d345a8bc0aa94d952ff6bb043c890c9322
Asuka Wang <hungmao.wang@linecorp.com>
2024-05-02 18:55:09 +0900

    feat: adjust post detail header layout

--- components/blog/PostDetailPageHeader.tsx
+++ components/blog/PostDetailPageHeader.tsx
@@ -9,12 +9,16 @@ interface Props {
 
 export const PostDetailPageHeader: FC<Props> = ({ postSummary }) => {
   return (
-    <div className="flex flex-col gap-fb3 mb-fb5">
+    <div className="flex flex-col gap-fb1 mb-fb8">
       <h1 className="text-fb13 font-abril leading-none">
         {postSummary.title.toUpperCase()}
       </h1>
-      <h2 className="text-fb5 italic font-gentium-basic">TODO: Post Description Things I learned in my first Godot project</h2>
-      <div className="text-fb3 font-noto-sans">{parseDateToEn(postSummary.publishDate)}</div>
+      <h2 className="text-fb5 italic font-gentium-basic">
+        TODO: Post Description Things I learned in my first Godot project
+      </h2>
+      <div className="text-fb3 font-noto-sans">
+        {parseDateToEn(postSummary.publishDate)}
+      </div>
     </div>
   );
 };

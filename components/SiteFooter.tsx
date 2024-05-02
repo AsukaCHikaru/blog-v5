dd240371c1f6f9d8132233e734a4a0d3ace1be51
Asuka Wang <hungmao.wang@linecorp.com>
2024-05-02 19:22:34 +0900

    feat: foot style

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -2,11 +2,12 @@ import { FC } from 'react';
 
 export const SiteFooter: FC = () => {
   return (
-    <div className="mt-6 mb-12 text-center col-span-12 lg:col-span-8 lg:col-start-3">
+    <div className="mt-fb8 pt-fb2 mb-fb8 font-noto-sans text-light75  border-t border-light50">
       <a
         href="https://asukachikaru.com"
         rel="noopener noreferrer"
         target="_blank"
+        className="text-fb2 hover:text-light"
       >
         asukachikaru.com
       </a>{' '}

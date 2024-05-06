1df9c175274ad522e312c29e8b4975d46c4b62a6
asukachikaru <asukachikaru@gmail.com>
2024-05-06 21:55:33 +0900

    feat: site footer mobile style

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const SiteFooter: FC = () => {
   return (
-    <div className="mt-fb8 pt-fb2 mb-fb8 font-noto-sans interactive-color border-t border-color">
+    <div className="mt-fb8 pt-fb2 mb-fb8 text-center lg:text-left font-noto-sans interactive-color border-t border-color">
       <a
         href="https://asukachikaru.com"
         rel="noopener noreferrer"

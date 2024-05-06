fb53631b13e5415a2c2ea5b6850a90bc86872b6d
asukachikaru <asukachikaru@gmail.com>
2024-05-06 22:17:22 +0900

    fix: table of content link path

--- components/blog/TableOfContentColumn.tsx
+++ components/blog/TableOfContentColumn.tsx
@@ -3,6 +3,7 @@ import { HeadingBlock } from 'types/markdown';
 import { SideColumnHeader } from './SideColumnHeader';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { ParsedUrlQuery, stringify } from 'querystring';
 
 interface Props {
   list: HeadingBlock[];
@@ -21,8 +22,14 @@ export const TableOfContentColumn: FC<Props> = ({ list }) => {
   );
 };
 
+const getPathname = (query: ParsedUrlQuery) => {
+  const stringifiedQuery = stringify(query);
+  return '/blog/' + (stringifiedQuery ? stringifiedQuery.split('=')[1] : '');
+};
+
 const HeaderLink: FC<{ block: HeadingBlock }> = ({ block }) => {
-  const { pathname } = useRouter();
+  const router = useRouter();
+  const pathname = getPathname(router.query);
   const text = block.children.map((item) => item.text);
   return (
     <li className="mt-fb2">

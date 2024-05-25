96555f138da4fef2837a433cc510ff06c9907084
asukachikaru <asukachikaru@gmail.com>
2024-05-25 19:09:32 +0900

    fix: table of contents link

--- components/blog/TableOfContentColumn.tsx
+++ components/blog/TableOfContentColumn.tsx
@@ -22,14 +22,18 @@ export const TableOfContentColumn: FC<Props> = ({ list }) => {
   );
 };
 
-const getPathname = (query: ParsedUrlQuery) => {
+const getPathname = (pathname: string, query: ParsedUrlQuery) => {
   const stringifiedQuery = stringify(query);
-  return '/blog/' + (stringifiedQuery ? stringifiedQuery.split('=')[1] : '');
+  console.log(stringifiedQuery);
+  return stringifiedQuery
+    ? pathname.replace('[pathname]', stringifiedQuery.split('=')[1])
+    : pathname;
 };
 
 const HeaderLink: FC<{ block: HeadingBlock }> = ({ block }) => {
   const router = useRouter();
-  const pathname = getPathname(router.query);
+  console.log(router.pathname);
+  const pathname = getPathname(router.pathname, router.query);
   const text = block.children.map((item) => item.text);
   return (
     <li className="mt-fb2">

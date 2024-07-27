5af396fcb3f4bfaeb6942c03f15dbbf3b5f07fac
asukachikaru <asukachikaru@gmail.com>
2024-07-27 19:45:06 +0900

    feat: add modern-normalize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeLayout } from '@components/layout/ThemeLayout';
 import { SiteHeader } from '@components/SiteHeader';
 import { SiteFooter } from '@components/SiteFooter';
 import { SiteLayout } from '@components/layout/SiteLayout';
 import { fonts } from 'styles/fonts';
+import 'styles/globals.css';
+import 'node_modules/modern-normalize/modern-normalize.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

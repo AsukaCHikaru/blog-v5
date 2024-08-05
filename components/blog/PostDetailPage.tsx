3f7496f7bacd7e52beab1ba6ee8982ac752d5d1b
asukachikaru <asukachikaru@gmail.com>
2024-08-05 19:31:57 +0900

    feat: post detail page base layout

--- components/blog/PostDetailPage.tsx
+++ components/blog/PostDetailPage.tsx
@@ -3,15 +3,13 @@ import { type CategoryList as CategoryListType, PostMetadata } from '@types';
 import { MarkdownBlock } from 'types/markdown';
 import { PostBodyBlock } from './PostBodyBlock';
 import { PostDetailPageHeader } from './PostDetailPageHeader';
-import { ContentLayout } from '@components/layout/ContentLayout';
-import { CategoryList } from './CategoryList';
-import { ArchiveList } from './ArchiveList';
 import { TableOfContentColumn } from './TableOfContentColumn';
-import { SideColumn } from './SideColumn';
 import { isHeadingBlock } from '@utils/markdownUtils';
-import { SideColumnHeader } from './SideColumnHeader';
-import { BelowPostDetailColumnHeader } from './BelowPostDetailColumnHeader';
-import styles from '@styles/blog/PostDetailPage.module.css';
+import {
+  MainContent,
+  RightPanel,
+  RightWideContent,
+} from '@components/layout/Layout';
 
 interface Props {
   categoryList: CategoryListType;
@@ -20,46 +18,22 @@ interface Props {
   last5posts: PostMetadata[];
 }
 
-export const PostDetailPage: FC<Props> = ({
-  categoryList,
-  postMetadata,
-  postDetail,
-  last5posts,
-}) => {
+export const PostDetailPage: FC<Props> = ({ postMetadata, postDetail }) => {
   const headers = postDetail.filter(isHeadingBlock);
 
   return (
-    <div>
-      <PostDetailPageHeader postMetadata={postMetadata} />
-      <ContentLayout>
-        <div className={styles['main-content-container']}>
-          {postDetail.map((block, i) => (
-            <PostBodyBlock block={block} key={i} />
-          ))}
-        </div>
-        <SideColumn>
-          {headers.length ? <TableOfContentColumn list={headers} /> : null}
-          <div>
-            <SideColumnHeader>ARCHIVE</SideColumnHeader>
-            <ArchiveList postList={last5posts} />
-          </div>
-          <div>
-            <SideColumnHeader>CATEGORY</SideColumnHeader>
-            <CategoryList categoryList={categoryList} />
-          </div>
-        </SideColumn>
-        <div className={styles['below-content-container']}>
-          <div className={`${styles['below-content-divider']} border-color`} />
-          <div className={styles['below-content-wrapper']}>
-            <BelowPostDetailColumnHeader>ARCHIVE</BelowPostDetailColumnHeader>
-            <ArchiveList postList={last5posts} />
-          </div>
-          <div className={styles['below-content-wrapper']}>
-            <BelowPostDetailColumnHeader>CATEGORY</BelowPostDetailColumnHeader>
-            <CategoryList categoryList={categoryList} />
-          </div>
-        </div>
-      </ContentLayout>
-    </div>
+    <>
+      <RightWideContent>
+        <PostDetailPageHeader postMetadata={postMetadata} />
+      </RightWideContent>
+      <MainContent>
+        {postDetail.map((block, i) => (
+          <PostBodyBlock block={block} key={i} />
+        ))}
+      </MainContent>
+      <RightPanel>
+        {headers.length ? <TableOfContentColumn list={headers} /> : null}
+      </RightPanel>
+    </>
   );
 };

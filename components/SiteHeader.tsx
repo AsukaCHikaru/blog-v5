a8f75072fbe85bf4bbc98f31efa30da8f40cb4a0
asukachikaru <asukachikaru@gmail.com>
2024-08-07 23:14:02 +0900

    feat: mobile menu base

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,10 +1,11 @@
 import { SECTIONS } from '../consts/sections';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useCallback } from 'react';
+import { ReactNode, useCallback, useState } from 'react';
 import styles from '@styles/SiteHeader.module.css';
 
 export const SiteHeader = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { pathname } = useRouter();
 
   const isLinkActive = useCallback(
@@ -17,26 +18,70 @@ export const SiteHeader = () => {
   );
 
   return (
-    <div className={styles.container}>
-      <div className={styles['nav-container']}>
-        <div className={styles['section-container']}>
-          {Object.values(SECTIONS).map((section) => (
-            <Link
-              key={section.url}
-              href={section.url}
-              className={`${styles['section-link']} interactive-color ${
-                isLinkActive(section) ? 'text-color' : ''
-              }`}
-            >
-              {section.label}
-            </Link>
-          ))}
+    <>
+      <div className={styles.container}>
+        <div className={styles['nav-container']}>
+          <div className={styles['section-container']}>
+            {Object.values(SECTIONS).map((section) => (
+              <Link
+                key={section.url}
+                href={section.url}
+                className={`${styles['section-link']} interactive-color ${
+                  isLinkActive(section) ? 'text-color' : ''
+                }`}
+              >
+                {section.label}
+              </Link>
+            ))}
+          </div>
+          <button
+            className={styles['menu-button']}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? 'close menu' : 'menu'}
+          </button>
+          <Link
+            href="/"
+            className={`${styles['publication-folio']} text-color`}
+          >
+            ASUKA WANG
+          </Link>
         </div>
-        <Link href="/" className={`${styles['publication-folio']} text-color`}>
-          ASUKA WANG
-        </Link>
+        <div className={`${styles['divider']} border-color-100`} />
       </div>
-      <div className={`${styles['divider']} border-color-100`} />
+      {isMenuOpen ? (
+        <>
+          <div
+            className={styles.backdrop}
+            onClick={() => setIsMenuOpen(false)}
+          />
+          <div className={styles.menu}>
+            <MenuSectionTitle active={pathname.includes('blog')}>
+              blog
+            </MenuSectionTitle>
+            <MenuSectionTitle active={pathname === '/about'}>
+              about
+            </MenuSectionTitle>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+const MenuSectionTitle = ({
+  children,
+  active,
+}: {
+  children: ReactNode;
+  active: boolean;
+}) => {
+  return (
+    <div
+      className={`${styles['menu-section-title']} interactive-color`}
+      data-active={active}
+    >
+      {children}
     </div>
   );
 };

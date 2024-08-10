d85d874e8d9058c5d5cea59162542a69354965fe
asukachikaru <asukachikaru@gmail.com>
2024-08-11 00:24:22 +0900

    feat: mobile menu section link

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import styles from '@styles/MobileMenu.module.css';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 interface Props {
   onClose: () => void;
@@ -13,12 +14,18 @@ export const MobileMenu = ({ onClose }: Props) => {
     <>
       <div className={styles.backdrop} onClick={() => onClose()} />
       <div className={styles.menu}>
-        <MenuSectionTitle active={pathname.includes('blog')}>
-          blog
-        </MenuSectionTitle>
-        <MenuSectionTitle active={pathname === '/about'}>
-          about
-        </MenuSectionTitle>
+        <MenuSectionTitle
+          active={pathname.includes('blog')}
+          label="blog"
+          path="/blog"
+          onTitleClick={onClose}
+        />
+        <MenuSectionTitle
+          active={pathname === '/about'}
+          label="about"
+          path="/about"
+          onTitleClick={onClose}
+        />
       </div>
     </>
   );
@@ -27,15 +34,26 @@ export const MobileMenu = ({ onClose }: Props) => {
 const MenuSectionTitle = ({
   children,
   active,
+  label,
+  path,
+  onTitleClick,
 }: {
-  children: ReactNode;
+  children?: ReactNode;
   active: boolean;
+  label: string;
+  path: string;
+  onTitleClick: () => void;
 }) => {
   return (
-    <div
-      className={`${styles['menu-section-title']} interactive-color`}
-      data-active={active}
-    >
+    <div data-active={active}>
+      <Link
+        href={path}
+        className={`${styles['menu-section-title']} interactive-color`}
+        data-active={active}
+        onClick={onTitleClick}
+      >
+        {label}
+      </Link>
       {children}
     </div>
   );

3316dd0fffd0e3139f585a9b13863a5e5dc8da06
asukachikaru <asukachikaru@gmail.com>
2024-08-10 13:09:27 +0900

    feat: generatePostTileList algorithm base

--- utils/blogUtils.ts
+++ utils/blogUtils.ts
@@ -10,34 +10,59 @@ export type PostTile = {
   pathname: string;
 };
 
-export const generatePostTileList = (posts: PostMetadata[]): PostTile[][] => {
-  const result: PostTile[][] = [];
+const TITLE_SIZE_2_THRESHOLD = 30;
+const DESCRIPTION_SIZE_2_THRESHOLD = 160;
 
-  let currentRow: PostTile[] = [];
-  posts.forEach((post) => {
-    const baseSize: TileSize = post.title.length > 30 ? 2 : 1;
-    if (currentRow.length === 0) {
-      currentRow.push({ size: baseSize, ...convertPostMetadataToTile(post) });
+export const generatePostTileList = (
+  posts: PostMetadata[],
+  rowSize: number = 4,
+): PostTile[][] => {
+  const result: PostTile[][] = [];
+  const getRowSize = (row: PostTile[]) =>
+    row.reduce((acc, cur) => acc + cur.size, 0);
+
+  const flatTileList = posts.map(convertPostMetadataToTile);
+
+  let row: PostTile[] = [];
+
+  flatTileList.forEach((tile) => {
+    if (row.length === 0) {
+      row.push(tile);
+
+      if (getRowSize(row) === rowSize) {
+        result.push([...row]);
+        row = [];
+      }
       return;
     }
-    currentRow.push({ size: baseSize, ...convertPostMetadataToTile(post) });
-    const currentRowSize = currentRow.reduce((acc, cur) => acc + cur.size, 0);
-    if (baseSize === 2 && currentRowSize === 3) {
-      currentRow[1].size = 2;
-    }
-    if (currentRowSize === 4) {
-      result.push([...currentRow]);
-      currentRow = [];
+
+    if (rowSize - getRowSize(row) === 1 && tile.size === 2) {
+      const size1Post = row.find((post) => post.size === 1);
+      if (size1Post) {
+        size1Post.size = 2;
+      }
+      result.push([...row]);
+      row = [tile];
       return;
+    }
+
+    row.push(tile);
+
+    if (getRowSize(row) === rowSize) {
+      result.push([...row]);
+      row = [];
     }
   });
 
   return result;
 };
 
-const convertPostMetadataToTile = (
-  post: PostMetadata,
-): Omit<PostTile, 'size'> => ({
+const convertPostMetadataToTile = (post: PostMetadata): PostTile => ({
+  size:
+    post.title.length > TITLE_SIZE_2_THRESHOLD ||
+    (post.description || '').length > DESCRIPTION_SIZE_2_THRESHOLD
+      ? 2
+      : 1,
   title: post.title,
   description: post.description || '',
   publishDate: post.publishDate,

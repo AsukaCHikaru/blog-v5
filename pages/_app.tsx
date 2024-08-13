3caf145da6b785cf210db49bb67cfe2e247fe4c9
asukachikaru <asukachikaru@gmail.com>
2024-08-13 21:18:04 +0900

    feat: init site context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,25 @@ import 'styles/globals.css';
 import 'styles/fonts.css';
 import 'node_modules/modern-normalize/modern-normalize.css';
 import { Layout } from '@components/layout/Layout';
+import { createContext } from 'react';
+
+type SiteContextType = {
+  activeSection: 'blog' | 'about' | undefined;
+  blogCategories: { name: string; count: number }[];
+};
+
+export const SiteContext = createContext<SiteContextType | null>(null);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SiteContext.Provider
+        value={{ activeSection: undefined, blogCategories: [] }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SiteContext.Provider>
     </main>
   );
 }

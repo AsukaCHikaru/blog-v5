b6991d2b9533bd8ccfbb9ebe8f02a1e03753ffd2
asukachikaru <asuka@asukawang.com>
2025-02-11 12:27:54 +0900

    feat: adjust quote block dom and style

--- components/blog/QuoteBlock.tsx
+++ components/blog/QuoteBlock.tsx
@@ -3,16 +3,12 @@ import { RichTextItem } from './PostBodyBlock';
 import styles from '@styles/blog/QuoteBlock.module.css';
 import { QuoteBlock as QuoteBlockType } from '@utils/markdownUtils';
 
-export const QuoteBlock: FC<{ block: QuoteBlockType }> = ({ block }) => {
-  return (
-    <div className={`${styles.container} text-color-second`}>
-      <span className={styles.wrapper}>
-        <span className={styles['quote-mark-start']}>&ldquo;</span>
-        {block.children.map((item, i) => (
-          <RichTextItem item={item} key={i} />
-        ))}
-        <span className={styles['quote-mark-end']}>&rdquo;</span>
-      </span>
-    </div>
-  );
-};
+export const QuoteBlock: FC<{ block: QuoteBlockType }> = ({ block }) => (
+  <blockquote className={`${styles.quoteblock} text-color-second`}>
+    {block.children.map((item, i) => (
+      <p key={i}>
+        <RichTextItem item={item} key={i} />
+      </p>
+    ))}
+  </blockquote>
+);

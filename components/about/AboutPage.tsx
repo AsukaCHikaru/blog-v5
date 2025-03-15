c246b678f148b2d4c502430de62046a779cbd2b6
asukachikaru <asuka@asukawang.com>
2025-03-15 11:22:08 +0900

    feat: remove table of contents

--- components/about/AboutPage.tsx
+++ components/about/AboutPage.tsx
@@ -1,33 +1,21 @@
 import { PostBodyBlock } from '@components/blog/PostBodyBlock';
-import { TableOfContentColumn } from '@components/blog/TableOfContentColumn';
-import {
-  FullContent,
-  MainContent,
-  RightPanel,
-} from '@components/layout/Layout';
-import { isHeadingBlock, MarkdownBlock } from '@utils/markdownUtils';
+import { FullContent, MainContent } from '@components/layout/Layout';
+import { MarkdownBlock } from '@utils/markdownUtils';
 import styles from '@styles/about/AboutPage.module.css';
 
-export const AboutPage = ({ content }: { content: MarkdownBlock[] }) => {
-  const headers = content.filter(isHeadingBlock);
-
-  return (
-    <>
-      <FullContent>
-        <div className={styles['header-wrapper']}>
-          <h1 className={styles.header}>About</h1>
-        </div>
-      </FullContent>
-      <MainContent>
-        <article className={styles.article}>
-          {content.map((block, i) => (
-            <PostBodyBlock block={block} key={i} />
-          ))}
-        </article>
-      </MainContent>
-      <RightPanel>
-        <TableOfContentColumn list={headers} />
-      </RightPanel>
-    </>
-  );
-};
+export const AboutPage = ({ content }: { content: MarkdownBlock[] }) => (
+  <>
+    <FullContent>
+      <div className={styles['header-wrapper']}>
+        <h1 className={styles.header}>About</h1>
+      </div>
+    </FullContent>
+    <MainContent>
+      <article className={styles.article}>
+        {content.map((block, i) => (
+          <PostBodyBlock block={block} key={i} />
+        ))}
+      </article>
+    </MainContent>
+  </>
+);
--- components/blog/PostDetailPage.tsx
+++ components/blog/PostDetailPage.tsx
@@ -1,17 +1,12 @@
 import { FC } from 'react';
 import { PostBodyBlock } from './PostBodyBlock';
 import { PostDetailPageHeader } from './PostDetailPageHeader';
-import { TableOfContentColumn } from './TableOfContentColumn';
 import {
   isHeadingBlock,
   MarkdownBlock,
   PostMetadata,
 } from '@utils/markdownUtils';
-import {
-  MainContent,
-  RightPanel,
-  FullContent,
-} from '@components/layout/Layout';
+import { MainContent, FullContent } from '@components/layout/Layout';
 import { AdditionalPostList } from './AdditionPostList';
 import styles from '@styles/blog/PostDetailPage.module.css';
 
@@ -27,37 +22,30 @@ export const PostDetailPage: FC<Props> = ({
   postContent,
   last5posts,
   categoryPosts = [],
-}) => {
-  const headers = postContent.filter(isHeadingBlock);
-
-  return (
-    <>
-      <FullContent>
-        <PostDetailPageHeader
-          title={postMetadata.title}
-          description={postMetadata.description}
-          publishDate={postMetadata.publishDate}
-        />
-      </FullContent>
-      <MainContent>
-        <article className={styles.article}>
-          {postContent.map((block, i) => (
-            <PostBodyBlock block={block} key={i} />
-          ))}
-        </article>
-        <div className={styles['additional-list-container']}>
-          {categoryPosts.length ? (
-            <AdditionalPostList
-              postList={categoryPosts}
-              category={postMetadata.category}
-            />
-          ) : null}
-          <AdditionalPostList postList={last5posts} />
-        </div>
-      </MainContent>
-      <RightPanel>
-        {headers.length ? <TableOfContentColumn list={headers} /> : null}
-      </RightPanel>
-    </>
-  );
-};
+}) => (
+  <>
+    <FullContent>
+      <PostDetailPageHeader
+        title={postMetadata.title}
+        description={postMetadata.description}
+        publishDate={postMetadata.publishDate}
+      />
+    </FullContent>
+    <MainContent>
+      <article className={styles.article}>
+        {postContent.map((block, i) => (
+          <PostBodyBlock block={block} key={i} />
+        ))}
+      </article>
+      <div className={styles['additional-list-container']}>
+        {categoryPosts.length ? (
+          <AdditionalPostList
+            postList={categoryPosts}
+            category={postMetadata.category}
+          />
+        ) : null}
+        <AdditionalPostList postList={last5posts} />
+      </div>
+    </MainContent>
+  </>
+);
